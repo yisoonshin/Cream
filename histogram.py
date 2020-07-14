785a2e585c0c631ff71def873d16f626e3d2facd
j-ffry <jeffry.valadez.94@gmail.com>
2020-07-14 16:01:09 -0400

    histogram normal vs mal

--- histogram.py
+++ histogram.py
@@ -12,16 +12,23 @@ import pandas
 #df = pandas.read_csv('simulation_scores.csv')
 df = pandas.read_csv('simulated_ddos_data.csv')
 magnitude = df['magnitude']
+normal_magnitude = df.loc[df.is_ddos==0,'magnitude']
+malicious_magnitude = df.loc[df.is_ddos==1, 'magnitude']
 
 
-def make_plot(title, hist, x):
+def make_plot(title, hist1, hist2):
     p = figure(title=title, tools="pan,box_select,zoom_in,zoom_out,save,reset", background_fill_color="#fafafa")
-    p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+    p.quad(top=hist1, bottom=0, left=edges[:-1], right=edges[1:],
            fill_color="orange", line_color="white", alpha=0.8)
+    p.quad(top=hist2, bottom=0, left=edges[:-1], right=edges[1:],
+          fill_color="blue", line_color="white", alpha=0.8)
     #p.line(x, pdf, line_color="#ff8888", line_width=4, alpha=0.7, legend_label="PDF")
     #p.line(x, cdf, line_color="orange", line_width=2, alpha=0.7, legend_label="CDF")
 
     p.y_range.start = 0
+    p.y_range.end = 100
+    p.x_range.start = 15000
+    p.x_range.end = 30000
     p.legend.location = "center_right"
     p.legend.background_fill_color = "#fefefe"
     p.xaxis.axis_label = 'Magnitude'
@@ -34,23 +41,16 @@ def make_plot(title, hist, x):
 
 # Normal Distribution
 
-mu, sigma = 0, 0.5
+hist1, edges = np.histogram(normal_magnitude, density=False, bins=100)
+hist2, edges = np.histogram(malicious_magnitude, density=False, bins=100)
 
-measured = np.random.normal(mu, sigma, 1000)
-
-hist, edges = np.histogram(magnitude, density=True, bins=100)
-
-x = np.linspace(-2, 2, 1000)
-#pdf = 1/(sigma * np.sqrt(2*np.pi)) * np.exp(-(x-mu)**2 / (2*sigma**2))
-#cdf = (1+scipy.special.erf((x-mu)/np.sqrt(2*sigma**2)))/2
-
-p1 = make_plot("Hypothetical Normal vs Malicious Activity Distribution", hist, x)
+p1 = make_plot("Hypothetical Normal vs Malicious Activity Distribution", hist1, hist2)
 
 
 
 output_file('histogram.html', title="histogram.py example")
 
-show(gridplot([p1], ncols=2, plot_width=400, plot_height=400, toolbar_location='right'))
+show(gridplot([p1], ncols=2, plot_width=500, plot_height=400, toolbar_location='right'))
 
 
 
